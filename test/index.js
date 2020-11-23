9636df6de387117a456e765da8bee45353f0f920
Bill Li <me@billxinli.com>
2020-11-23 18:33:59 -0500

    feat(subpath): allow subpath to be used for server

--- test/index.js
+++ test/index.js
@@ -97,3 +97,59 @@ test('global defaults', assert => {
     query: { is_good: 'yes' }
   })
 })
+
+test('sub path in server', assert => {
+  assert.plan(1)
+
+  http.callsFake(options => {
+    assert.deepEqual(options, {
+      protocol: 'https',
+      port: '',
+      host: 'pets.com',
+      method: 'get',
+      path: '/api/v1-0-0/pets/1?name=ruby&is_good=yes',
+      headers: { 'x-pet-type': 'dog' },
+      body: undefined
+    })
+  })
+
+  const API = client(spec)
+
+  const api = new API('https://pets.com/api/v1-0-0', {
+    headers: { 'x-pet-type': 'dog' },
+    params: { petId: 1 },
+    query: { name: 'ruby' }
+  })
+
+  api.showPetById({
+    query: { is_good: 'yes' }
+  })
+})
+
+test('sub path in server without slashes', assert => {
+  assert.plan(1)
+
+  http.callsFake(options => {
+    assert.deepEqual(options, {
+      protocol: 'https',
+      port: '',
+      host: 'pets.com',
+      method: 'get',
+      path: '/api/v1-0-0/pets/1?name=ruby&is_good=yes',
+      headers: { 'x-pet-type': 'dog' },
+      body: undefined
+    })
+  })
+
+  const API = client(spec)
+
+  const api = new API('https://pets.com/api/v1-0-0/', {
+    headers: { 'x-pet-type': 'dog' },
+    params: { petId: 1 },
+    query: { name: 'ruby' }
+  })
+
+  api.showPetById({
+    query: { is_good: 'yes' }
+  })
+})
